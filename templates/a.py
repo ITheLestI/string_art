d0da9a238c959d8f116403e74043d7c0013544e4
ITheLestI <pmoskvin2005@ya.ru>
2021-08-11 12:25:50 +0300

    123

--- templates/a.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-files = {
-    'file': ('t_source.jpg', open('t_source.jpg', 'rb')),
-}
-
-#response = requests.get("http://localhost:5000/")
-response = requests.post('http://localhost:5000/', files=files)
-#print(response.content)
