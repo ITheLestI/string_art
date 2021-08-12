5fc442cc018d3b8fe6d99e82015944aeaf0ae561
agapiton <nikwitos2007@gmail.com>
2021-08-12 20:08:21 +0300

    checkbox

--- script.js
+++ script.js
@@ -1,18 +1,66 @@
-// function synchronizationPercentText() {
-//     if (document.getElementById("percentRange").value != document.getElementById("percentText").value) {
-//         document.getElementById("percentRange").value = document.getElementById("percentText").value
-//     }
-// }
-// function synchronizationPercentRange() {
-//     if (document.getElementById("percentRange").value != document.getElementById("percentText").value) {
-//         document.getElementById("percentText").value = document.getElementById("percentRange").value
-//     }
-// }
+const ip = 'http://192.168.239.16:5000/'
+canv = document.getElementById("canvas");
+ctx = canv.getContext("2d");
+function checkbox() {
+    if (result) {
+        if (document.getElementById("checkbox").checked) {
+            createInstructionButtons()
+            document.getElementById("currectline").innerText=currectLine
+            clearCanvas()
+            ctx.moveTo(result[currectLine][0][0], result[currectLine][0][1])
+            ctx.lineTo(result[currectLine][1][0], result[currectLine][1][1])
+            ctx.stroke()
+        }
+        else {
+            deleteButtons()
+            document.getElementById("currectLine").innerText=null
+            ctx.lineWidth = document.getElementById("lineWidthRange").value
+            for (var i = 0; i < result.length; i++) {
+                ctx.moveTo(result[i][0][0], result[i][0][1]);
+                ctx.lineTo(result[i][1][0], result[i][1][1]);
+                
+                // ctx.lineWidth = document.getElementById("lineWidthRange").value
+            }
+            ctx.stroke()
+        }
+    }
+}
+function increment() {
+    if (currectLine < result.length - 1) {
+        currectLine++
+        document.getElementById("currectLine").innerText = currectLine + 1
+        clearCanvas()
+        ctx.moveTo(result[currectLine][0][0], result[currectLine][0][1])
+        ctx.lineTo(result[currectLine][1][0], result[currectLine][1][1])
+        ctx.stroke()
+    }
+}
+function decrement() {
+    if (currectLine > 0) {
+        currectLine--
+        document.getElementById("currectLine").innerText = currectLine + 1
+        clearCanvas()
+        ctx.moveTo(result[currectLine][0][0], result[currectLine][0][1])
+        ctx.lineTo(result[currectLine][1][0], result[currectLine][1][1])
+        ctx.stroke()
+    }
+}
+function createInstructionButtons() {
+    document.getElementById("instructionButtonNext").innerHTML = "<button>next</button>"
+    document.getElementById("instructionButtonPrevious").innerHTML = "<button>previous</button>"
+    document.getElementById("currectLine").innerText = currectLine + 1
+    document.getElementById("instructionButtonPrevious").setAttribute("onclick", "decrement()")
+    document.getElementById("instructionButtonNext").setAttribute("onclick", "increment()")
+}
+function deleteButtons() {
+    document.getElementById("instructionButtonNext").innerHTML = ""
+    document.getElementById("instructionButtonPrevious").innerHTML = ""
+}
+var currectLine
 function synchronizationlineWidthRange() {
     if (document.getElementById("lineWidthRange").value != document.getElementById("lineWidthText").value) {
         document.getElementById("lineWidthText").value = document.getElementById("lineWidthRange").value
         ctx.lineWidth = document.getElementById("lineWidthRange").value;
-        console.log(ctx.lineWidth)
     }
 }
 function synchronizationlineWidthText() {
@@ -39,8 +87,7 @@ function clearCanvas() {
     ctx.stroke()
     ctx.setLineDash([0])
 }
-canv = document.getElementById("canvas");
-ctx = canv.getContext("2d");
+
 ctx.beginPath()
 ctx.arc(190, 190, 190, 0, (2 * Math.PI), false);
 ctx.setLineDash([3]);
@@ -58,26 +105,25 @@ function change() {
     }
 }
 function afterResponse(succes) {
+    if (document.getElementById("checkbox").checked) {
+        createInstructionButtons()
+    }
     document.getElementById("loader").setAttribute("class", "y")
     result = succes
     clearCanvas()
     ctx.lineWidth = document.getElementById("lineWidthRange").value
-    for (var i = 0; i < result.length; i++) {
-        console.log(ctx.lineWidth)
-        ctx.moveTo(result[i][0][0], result[i][0][1]);
-        ctx.lineTo(result[i][1][0], result[i][1][1]);
-        // ctx.lineWidth = document.getElementById("lineWidthRange").value
-    }
+    checkbox()
     ctx.stroke()
     ctx.closePath()
-
 }
+
 // Select your input type file and store it in a variable
 const input = document.getElementById('file');
 console.log('i', input)
 
 // This will upload the file after having read it
 const upload = (file) => {
+    currectLine = 0
     clearCanvas()
     document.getElementById("loader").setAttribute("class", "loader")
     var settings = [document.getElementById("lineNumText").value]
@@ -86,7 +132,7 @@ const upload = (file) => {
     formData.append('json', JSON.stringify(settings));
     console.log("sending")
 
-    fetch('http://192.168.245.94:5000/', { // Your POST endpoint
+    fetch(ip, { // Your POST endpoint
         method: 'POST',
         body: formData, // This is your file object
         // headers: {
@@ -109,7 +155,7 @@ const onSelectFile = () => {
     if (input.files[0]) {
         upload(input.files[0])
     }
-    else{
+    else {
         alert("Выберите файл")
     }
 };
@@ -120,7 +166,5 @@ submit.addEventListener("click", onSelectFile, false)
 // Add a listener on your input
 // It will be triggered when a file will be selected
 
-if (result) {
-    console.log(result)
-}
+
 // [[[откуда][куда]],[[][]],[[][]],[[][]]]
